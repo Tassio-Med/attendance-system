576002434e61c87b6e763392c31cd865452bb5fc
Tassio-Med <tassiohfmed@gmail.como>
2023-01-15 17:42:41 -0300

    :recycle:refactor: mudança de planos na cofiguração de rotas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-// import firebase from './services/firebaseConnection';
-import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes/>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes/>
+    </BrowserRouter>
   );
 }
 
