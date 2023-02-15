8df9065082246cef0a40a8fa7bc143075228f3a0
Tassio-Med <tassiohfmed@gmail.como>
2023-02-15 15:32:20 -0300

    :sparkles: feat: header adicionado ao Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,13 @@
 import { useContext } from 'react';
 import { AuthContext } from '../../contexts/auth';
+import Header from '../../components/Header';
 
 export default function Dashboard() {
   const {signOut} = useContext(AuthContext);
 
   return(
     <div>
+      <Header/>
       <h1>PÁGINA DE DASHBOARD</h1>
       <button onClick={ () => signOut() }>Sair</button>
     </div>
