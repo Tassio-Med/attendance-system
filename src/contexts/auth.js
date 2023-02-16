cf62db2ad470727fc363b0be0f47aa6bea6c4e0a
Tassio-Med <tassiohfmed@gmail.como>
2023-02-16 18:50:58 -0300

    :sparkles: feat: atualização de nome e imagem do perfil em comunicação com o banco de dados

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -132,7 +132,9 @@ function AuthProvider({ children }){
       signUp,
       signOut,
       signIn,
-      loadingAuth
+      loadingAuth,
+      setUser,
+      storageUser
     }}
     >
       {children}
