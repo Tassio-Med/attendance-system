867b525572a7b6964d2d9e4200028ebe6c66637d
Tassio-Med <tassiohfmed@gmail.como>
2023-02-20 16:17:48 -0300

    :sparkles: feat: tabela de atendimento criada estruturada

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,13 +3,14 @@ import './dashboard.css';
 // import { AuthContext } from '../../contexts/auth';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { BsListTask, BsPlusLg } from 'react-icons/bs';
+import { BsListTask, BsPlusLg, BsSearch } from 'react-icons/bs';
+import { FiEdit2 } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 export default function Dashboard() {
   // const {signOut} = useContext(AuthContext);
-  const [chamados, setChamados] = useState([]);
+  const [chamados, setChamados] = useState([1]);
 
   return(
     <div>
@@ -35,6 +36,36 @@ export default function Dashboard() {
              <BsPlusLg size={15} color="#fff"/>
              Novo chamado
            </Link>
+
+           <table>
+              <thead>
+                <tr>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assunto</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Cadastrado em</th>
+                  <th scope="col">#</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label="Cliente">Sujeito</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span className="badge" style={{backgroundColor: '#5cb85c' }}>Em aberto</span>
+                  </td>
+                  <td data-label="Cadastrado">20/06/2021</td>
+                  <td data-label="#">
+                    <button className="action" style={{backgroundColor: '#3583f6' }}>
+                      <BsSearch color="#FFF" size={17} />
+                    </button>
+                    <button className="action" style={{backgroundColor: '#F6a935' }}>
+                      <FiEdit2 color="#FFF" size={17} />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </>
         )}
       </div>
