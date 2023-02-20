dd1b0a6c1708cca50b93d685d43f8c5f44af5566
Tassio-Med <tassiohfmed@gmail.como>
2023-02-20 15:39:41 -0300

    :sparkles: feat: Página inicial do Dashboard sem sem nenhum chamado criada.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,43 @@
-import { useContext } from 'react';
-import { AuthContext } from '../../contexts/auth';
+import './dashboard.css';
+// import { useContext } from 'react';
+// import { AuthContext } from '../../contexts/auth';
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { BsListTask, BsPlusLg } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Dashboard() {
-  const {signOut} = useContext(AuthContext);
+  // const {signOut} = useContext(AuthContext);
+  const [chamados, setChamados] = useState([]);
 
   return(
     <div>
       <Header/>
-      <h1>PÁGINA DE DASHBOARD</h1>
+      <div className='content'>
+        <Title name="Atendimentos">
+          <BsListTask size={25}/>
+        </Title>
+
+        {chamados.length === 0 ? (
+          <div className='container dashboard'>
+           <span>Nenhum chamado registrado...</span>
+
+           <Link className='new'>
+             <BsPlusLg size={15} color="#fff"/>
+             Novo chamado
+           </Link>
+          </div>
+          
+        ) : (
+          <>
+            <Link className='new'>
+             <BsPlusLg size={15} color="#fff"/>
+             Novo chamado
+           </Link>
+          </>
+        )}
+      </div>
     </div>
   )
 }
