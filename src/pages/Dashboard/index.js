77e135ff1d442397dbcfe76376745c86a5af890b
Tassio-Med <tassiohfmed@gmail.como>
2023-02-21 17:36:17 -0300

    :sparkles: feat: dashbord fazendo conexão e buscando chamados no firebase

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,16 +1,77 @@
 import './dashboard.css';
+import { BsListTask, BsPlusLg, BsSearch } from 'react-icons/bs';
+import { FiEdit2 } from 'react-icons/fi';
 // import { useContext } from 'react';
 // import { AuthContext } from '../../contexts/auth';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { BsListTask, BsPlusLg, BsSearch } from 'react-icons/bs';
-import { FiEdit2 } from 'react-icons/fi';
+
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import { format } from 'date-fns/esm';
+
+import firebase from '../../services/firebaseConnection';
+
+const listRef = firebase.firestore().collection('chamados').orderBy('created', 'desc');
 
 export default function Dashboard() {
   // const {signOut} = useContext(AuthContext);
-  const [chamados, setChamados] = useState([1]);
+  const [chamados, setChamados] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [lastDocs, setLastDocs] = useState();
+
+  useEffect(() => {
+    loadChamados();
+    return () => {
+
+    }
+  }, []);
+
+  async function loadChamados(){
+    await listRef.limit(5)
+    .get()
+    .then((snapshot) => {
+      updateState(snapshot)
+    })
+    .catch((err) => {
+      console.log('Deu algum erro: ', err);
+      setLoadingMore(false);
+    })
+
+    setLoading(false);
+  }
+
+  async function updateState(snapshot){
+    const isCollectionEmpty = snapshot.size === 0;
+
+    if(!isCollectionEmpty){
+      let lista = [];
+
+      snapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          assunto: doc.data().assunto,
+          cliente: doc.data().cliente,
+          clienteId: doc.data().clienteId,
+          created: doc.data().created,
+          createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+          status: doc.data().status,
+          complemento: doc.data().complemento
+        })
+      })
+
+      const lastDoc = snapshot.docs[snapshot.docs.length -1];
+      
+      setChamados( chamados => [...chamados,...lista]);
+      setLastDocs(lastDoc);
+    } else {
+      setIsEmpty(true);
+    }
+    setLoadingMore(false);
+  }
 
   return(
     <div>
