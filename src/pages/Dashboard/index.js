44a58fd3ec93c40333179069ce2206857add8b2f
Tassio-Med <tassiohfmed@gmail.como>
2023-02-23 17:18:22 -0300

    :sparkles: feat: reacebe o Modal para renderizar

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
+import Modal from '../../components/Modal';
 import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
@@ -19,8 +20,27 @@ export default function Dashboard(){
   const [isEmpty, setIsEmpty] = useState(false);
   const [lastDocs, setLastDocs] = useState();
 
+  const [showPostModal, setShowPostModal] = useState(false); 
+  const [detail, setDetail] = useState();
+  
+
 
   useEffect(()=> {
+
+    async function loadChamados(){
+      await listRef.limit(5)
+      .get()
+      .then((snapshot) => {
+        updateState(snapshot)
+      })
+      .catch((err)=>{
+        console.log('Deu algum erro: ', err);
+        setLoadingMore(false);
+      })
+  
+      setLoading(false);
+  
+    }
 
     loadChamados();
 
@@ -30,22 +50,6 @@ export default function Dashboard(){
   }, []);
 
   
-  async function loadChamados(){
-    await listRef.limit(5)
-    .get()
-    .then((snapshot) => {
-      updateState(snapshot)
-    })
-    .catch((err)=>{
-      console.log('Deu algum erro: ', err);
-      setLoadingMore(false);
-    })
-
-    setLoading(false);
-
-  }
-
-
   async function updateState(snapshot){
     const isCollectionEmpty = snapshot.size === 0;
 
@@ -86,6 +90,12 @@ export default function Dashboard(){
     .then((snapshot)=>{
       updateState(snapshot)
     })
+  }
+
+  function togglePostModal(item){
+    setShowPostModal(!showPostModal)
+    setDetail(item);
+
   }
  
 
@@ -154,7 +164,7 @@ export default function Dashboard(){
                       </td>
                       <td data-label="Cadastrado">{item.createdFormated}</td>
                       <td data-label="#">
-                        <button className="action" style={{backgroundColor: '#3583f6' }}>
+                        <button className="action" style={{backgroundColor: '#3583f6' }} onClick={ () => togglePostModal(item) }>
                           <FiSearch color="#FFF" size={17} />
                         </button>
                         <button className="action" style={{backgroundColor: '#F6a935' }}>
@@ -174,6 +184,13 @@ export default function Dashboard(){
         )}
 
       </div>
+
+      {showPostModal && (
+        <Modal
+          conteudo={detail}
+          close={togglePostModal}
+        />
+      )}
 
     </div>
   )
